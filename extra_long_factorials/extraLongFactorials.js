d0af1eb0854d1ddd101fdc2462116ab273d92544
Div <divyeshpatel685@gmail.com>
2020-06-07 23:10:39 +0300

    factorials

---
+++ extra_long_factorials/extraLongFactorials.js
@@ -0,0 +1,10 @@
+function extraLongFactorials(n) {
+  let number = BigInt(n);
+  let j = 1n;
+  for (let i = number; i > 0; i--) {
+    j *= i;
+  }
+  console.log(String(j));
+}
+
+extraLongFactorials(45);
